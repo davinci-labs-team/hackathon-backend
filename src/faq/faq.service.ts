f7b0790eb1671b609440cc4b5bc97f8a1b636b54
Elena Villepreux <elena.villepreux@epita.fr>
2025-09-08 12:13:41 +0200

    O0002: faq service and controller

--- src/faq/faq.service.ts
+++ src/faq/faq.service.ts
@@ -1,22 +1,27 @@
 import {
     Injectable,
     NotFoundException,
+    ForbiddenException
 } from "@nestjs/common";
 import { CreateFaqDto } from "./dto/create-faq.dto";
 import { UpdateFaqDto } from "./dto/update-faq.dto";
 import { UUID } from "crypto";
 import { PrismaService } from "../prisma/prisma.service";
 import { FaqResponse } from "./dto/faq-response";
+import { Role } from "@prisma/client";
 
 @Injectable()
 export class FaqService {
     constructor(private readonly prisma: PrismaService) {}
 
-    async create(createFaqDto: CreateFaqDto): Promise<FaqResponse> {
+    async create(createFaqDto: CreateFaqDto, supabaseUserId: string): Promise<FaqResponse> {
+        await this.validateUserRole(supabaseUserId);
+
         return await this.prisma.faq.create({
             data: {
                 question: createFaqDto.question,
                 answer: createFaqDto.answer,
+                isPrivate: createFaqDto.isPrivate,
             },
         });
     }
@@ -37,7 +42,9 @@ export class FaqService {
         return faq;
     }
 
-    async update(id: UUID, updateFaqDto: UpdateFaqDto): Promise<FaqResponse> {
+    async update(id: UUID, updateFaqDto: UpdateFaqDto, supabaseUserId: string): Promise<FaqResponse> {
+        await this.validateUserRole(supabaseUserId);
+
         // Check if the FAQ exists
         const faq = await this.prisma.faq.findUnique({
             where: { id },
@@ -51,11 +58,14 @@ export class FaqService {
             data: {
                 question: updateFaqDto.question,
                 answer: updateFaqDto.answer,
+                isPrivate: updateFaqDto.isPrivate,
             },
         });
     }
 
-    async remove(id: UUID): Promise<FaqResponse> {
+    async remove(id: UUID, supabaseUserId: string): Promise<FaqResponse> {
+        await this.validateUserRole(supabaseUserId);
+
         // Check if the FAQ exists
         const faq = await this.prisma.faq.findUnique({
             where: { id },
@@ -67,5 +77,17 @@ export class FaqService {
         return await this.prisma.faq.delete({
             where: { id },
         });
+    }
+
+    private async validateUserRole(supabaseUserId: string) {
+        const existUser = await this.prisma.user.findUnique({
+            where: { supabaseUserId },
+        });
+        if (!existUser) {
+            throw new NotFoundException("User not found.");
+        }
+        if (existUser?.role !== Role.ORGANIZER) {
+            throw new ForbiddenException("Only organizer can create FAQs.");
+        }
     }
 }
