5a4b3956db5b1b01614e574581879e88bacf14d2
Hugo Saint-Raymond <67425965+Gollumgogo@users.noreply.github.com>
2026-01-22 18:45:56 +0100

    feat: add image auto deleting for anouncements and profile (#21)

--- src/s3-bucket/s3-bucket.module.ts
+++ src/s3-bucket/s3-bucket.module.ts
@@ -1,7 +1,8 @@
-import { Module } from "@nestjs/common";
+import { Global, Module } from "@nestjs/common";
 import { S3BucketService } from "./s3-bucket.service";
 import { S3BucketController } from "./s3-bucket.controller";
 
+@Global()
 @Module({
   controllers: [S3BucketController],
   providers: [S3BucketService],
