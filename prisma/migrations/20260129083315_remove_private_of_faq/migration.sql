7313a48a9cfd1090448f7134c7634f0696d58468
Leo Boismartel <leo.boismartel@epita.fr>
2026-01-29 09:34:33 +0100

    refactor: remove `isPrivate` field from the `Faq` model and table.

---
+++ prisma/migrations/20260129083315_remove_private_of_faq/migration.sql
@@ -0,0 +1,8 @@
+/*
+  Warnings:
+
+  - You are about to drop the column `isPrivate` on the `Faq` table. All the data in the column will be lost.
+
+*/
+-- AlterTable
+ALTER TABLE "Faq" DROP COLUMN "isPrivate";
